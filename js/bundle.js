79791c46829a010586766dc4bfdfccdb662ba426
AnastasiaBar <90132016+AnastasiaBar@users.noreply.github.com>
2023-12-22 14:25:58 +0300

    Updates

---
+++ js/bundle.js
@@ -0,0 +1,2 @@
+!function(){var r={"./src/js/script.js":function(){}},s={};function t(n){var o=s[n];if(void 0!==o)return o.exports;var c=s[n]={exports:{}};return r[n](c,c.exports,t),c.exports}t("./src/js/script.js")}();
+//# sourceMappingURL=bundle.js.map
