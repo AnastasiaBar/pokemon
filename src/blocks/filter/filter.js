55e0235ca192831dc4f171ad5ec31925e411fb21
AnastasiaBar <90132016+AnastasiaBar@users.noreply.github.com>
2023-12-26 16:01:27 +0300

    feat: адаптив

--- src/blocks/filter/filter.js
+++ src/blocks/filter/filter.js
@@ -1,5 +1,18 @@
-// import ready from "../../js/utils/documentReady.js";
+import ready from "../../js/utils/documentReady.js";
 
-// ready(function () {
-//
-// });
+ready(function () {
+  const menuTrigger = document.querySelector(".filter__burger-btn");
+  menuTrigger.addEventListener("click", openNav);
+});
+
+function openNav() {
+  let bodyState = document.body.getAttribute("data-state");
+  const filter = document.querySelector(".overlay");
+  if (bodyState === "mobile-menu") {
+    document.body.dataset.state = "";
+    filter.classList.remove("overlay--active");
+  } else {
+    document.body.dataset.state = "mobile-menu";
+    filter.classList.add("overlay--active");
+  }
+}
