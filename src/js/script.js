30f962219a2cc10718bdd3bb6327df63cfd5887f
AnastasiaBar <90132016+AnastasiaBar@users.noreply.github.com>
2024-01-31 09:46:58 +0300

    add: скелетон

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,24 @@
-window.onload = function () {
-  const pageLoader = document.querySelector(".page__loader");
-  const loader = document.querySelector(".page__loader .loader");
+import { loadCard } from "../blocks/container-cards/container-cards";
 
-  pageLoader.classList.add("page__loader--active");
-  loader.classList.add("loader--active");
+window.onload = function () {
+  const elements = document.querySelectorAll(".skeleton");
+
+  elements.forEach((item) => {
+    const containerClass = nameClass(item);
+    item.classList.add(`${containerClass}__skeleton--active`);
+  });
 
   setTimeout(function () {
-    pageLoader.classList.remove("page__loader--active");
-    loader.classList.remove("loader--active");
-  }, 2000);
+    elements.forEach((item) => {
+      const containerClass = nameClass(item);
+      item.classList.remove(`${containerClass}__skeleton--active`);
+    });
+    loadCard();
+  }, 4000);
 };
+
+function nameClass(item) {
+  const result = item.className.split(" ")[0].toLowerCase();
+  const uniqueWords = [...new Set(result.split(" "))];
+  return uniqueWords.join(" ");
+}
